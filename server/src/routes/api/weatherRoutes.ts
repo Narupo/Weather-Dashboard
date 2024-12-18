7e9d38163e0dc816dad5fdeaab08282ea894e135
Narupo <pouzumaki@gmail.com>
2024-12-18 15:57:00 -0600

    updated the functionality of weatherRoutes.ts

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,4 +1,6 @@
 import { Router, type Request, type Response } from 'express';
+import { HistoryService} from '../../service/historyService.js';
+import { WeatherService } from '../../service/weatherService.js';
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
@@ -7,7 +9,20 @@ const router = Router();
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req: Request, res: Response) => {
   // TODO: GET weather data from city name
+  const {cityName} = req.body;
+  if (!cityName) {
+    return res.status(400).json({message: 'City name is required'});
+  }
+  try {
+    const weatherData = await WeatherService.getWeatherData(cityName);
   // TODO: save city to search history
+  await HistoryService.addCity(cityName);
+
+  res.json(weatherData);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    res.status(500).json({message: 'Failed to fetch weather data'});
+  }
 });
 
 // TODO: GET search history
